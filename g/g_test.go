b1b80919761c7cb9233a356fff28e426a1dd886d
TimAndy <xuchonglei@126.com>
2022-04-11 19:23:06 +0800

    Add testcase for package g

---
+++ g/g_test.go
@@ -0,0 +1,57 @@
+// Copyright 2022 TimAndy. All rights reserved.
+// Licensed under the Apache-2.0 license that can be found in the LICENSE file.
+
+package g
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+func TestGetgp(t *testing.T) {
+	gp0 := getgp()
+	runtime.GC()
+	assert.NotNil(t, gp0)
+	//
+	runTest(t, func() {
+		gp := getgp()
+		runtime.GC()
+		assert.NotNil(t, gp)
+		assert.NotEqual(t, gp0, gp)
+	})
+}
+
+func TestGetg0(t *testing.T) {
+	runTest(t, func() {
+		g0 := getg0()
+		runtime.GC()
+		stackguard0 := reflect.ValueOf(g0).FieldByName("stackguard0")
+		assert.Greater(t, stackguard0.Uint(), uint64(0))
+	})
+}
+
+func TestGetgt(t *testing.T) {
+	runTest(t, func() {
+		gt := getgt()
+		runtime.GC()
+		assert.Equal(t, "g", gt.Name())
+		//
+		assert.Greater(t, gt.NumField(), 20)
+	})
+}
+
+func runTest(t *testing.T, fun func()) {
+	run := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		fun()
+		run = true
+		wg.Done()
+	}()
+	wg.Wait()
+	assert.True(t, run)
+}
